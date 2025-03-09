cd7b347429284fc47f7ce272d54a1e7a444d4096
Joe Chirchirillo <cchirchirillo3@gmail.com>
2025-03-09 15:44:45 -0600

    Chore: controller creation and CRUD routes

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,31 @@
+class NotesController < ApplicationController
+  def index
+    if params[:subject_id]
+      subject = Subject.find(params[:subject_id])
+      @notes = subject.notes
+    else
+      @notes = Note.all
+    end
+    render json: @notes
+  end
+
+  def show
+    @note = Note.find(params[:id])
+    render json: @note, include: :subjects
+  end
+
+  def create
+    @note = Note.new(note_params)
+    if @note.save
+      render json: @note, status: :created
+    else
+      render json: { errors: @note.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title, :content, subject_ids: [])
+  end
+end
